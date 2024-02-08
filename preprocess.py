e25fed81a2db81c4292b5c6030a5444204a40a7e
qiud1 <qdavid472@gmail.com>
2024-02-08 02:37:18 -0500

    Organize Config Files

--- preprocess.py
+++ preprocess.py
@@ -6,22 +6,22 @@ NDVI = lambda x:str(x).split('_')[-2]
 GPM = lambda x: str(x).split('.')[-4][:8]
 GLDAS = lambda x: str(x).split('_')[2][:~4]
 
-CONFIG_LIST = ["NDVI_config.json", "GPM_config.json", "GLDAS_config.json"]
+CONFIG_LIST = ["Configs/NDVI_config.json", "Configs/GPM_config.json", "Configs/GLDAS_config.json"]
 KWARGS_LIST = [
     {
         "record_out": NDVI,
         "region_out": REGION,
-        "nc_config": "NDVI_nc.json"
+        "nc_config": "Configs/NDVI_nc.json"
     },
     {
         "record_out": GPM,
         "region_out": REGION,
-        "nc_config": "GPM_nc.json"
+        "nc_config": "Configs/GPM_nc.json"
     },
     {
         "record_out": GLDAS,
         "region_out": REGION,
-        "nc_config": "GLDAS_nc.json"
+        "nc_config": "Configs/GLDAS_nc.json"
     }
 ]
 
@@ -40,5 +40,4 @@ def process_all(CONFIG_LIST, KWARGS_LIST):
 
 
 if __name__ == "__main__":
-    # process_one("GLDAS_config.json", 2)
     process_all(CONFIG_LIST, KWARGS_LIST)
